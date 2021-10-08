f1e9f3823098d996f3b5feeb849123e4b4f092d6
Evan Fawcett <efawcett16@gmail.com>
2021-10-08 13:26:19 -0400

    initial commit with Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region
+    public static GameManager gm;
+
+    void Awake()
+    {
+        if (!gm)
+            gm = this;
+        else
+            Destroy(this);
+    }
+    #endregion
+
+    public GameObject player;
+
+    [HideInInspector]
+    public PlayerController pc;
+
+    void Start()
+    {
+        pc = player.GetComponent<PlayerController>();
+    }
+}
